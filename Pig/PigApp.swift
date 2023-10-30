6a7b1d0d4c4b28d7da6f86c6a5d0251e92200ae4
Phillip Mantatsky <pmantatsky1@bsd220.org>
2023-10-30 09:26:53 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Phillip Mantatsky on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
