375fe9f661065cb87f5d521a3569ccceddfd02cd
Phillip Mantatsky <pmantatsky1@bsd220.org>
2023-10-30 09:50:40 -0500

    Added die Image view

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct ContentView: View {
     @State var turnScore = 0
     @State var gameScore = 0
+    @State var randomValue = 0
+    @State var rotation = 0.0
     var body: some View {
         ZStack{
             Color.gray.opacity(0.7).ignoresSafeArea()
             VStack {
                 Image("Pig").resizable().frame(width: 150, height: 150)
                 CustomText(text: "Pig")
+                Image("Pips \(randomValue)")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .rotationEffect(.degrees(rotation))
+                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 1))
+                    .padding(50)
                 CustomText(text: "Turn Score: \(turnScore)")
                 CustomText(text: "Game Score: \(gameScore)")
                 Spacer()
